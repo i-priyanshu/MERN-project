0c571043d36d346d3033d63e7d40635249c8e975
i-priyanshu <i.priyanshukumar01@gmail.com>
2022-06-04 20:07:52 +0530

    Initilization

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/home/home';
+import List from './pages/list/List';
+import Hotels from './pages/Hotels/Hotel';
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/hotels" element={<List/>}/>
+          <Route path="/hotels/:id" element={<Hotels/>}/>
+        </Routes>
+      </BrowserRouter>
+  );
 };
 
 export default App;
