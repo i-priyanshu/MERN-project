933c48ab12b3c5ca5399f29e62bc204ce2514c50
i-priyanshu <i.priyanshukumar01@gmail.com>
2022-06-24 22:31:58 +0530

    Added more pages

---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,13 @@
+import "./propertylist.css";
+
+const PropertyList = () => {
+  return (
+    <div className='pList'>
+      <div className='pListItem'>
+        <img src='' alt='' className='pListImg' />
+      </div>
+    </div>
+  );
+};
+
+export default PropertyList;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,3 +1,4 @@
+import Featured from "../../components/featured/Featured";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.css";
@@ -7,7 +8,10 @@ const Home = () => {
     <>
       <Navbar />
       <Header />
-      <div className='homeContainer'></div>
+      <div className='homeContainer'>
+        <Featured />
+        <h1 className='homeTitle'>Browse by property type</h1>
+      </div>
     </>
   );
 };
